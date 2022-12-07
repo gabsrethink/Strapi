5f16b1c6b4f168ad404ac4e967089f948a695a9e
gabsrethink <gabriel.melo@rethink.dev>
2022-12-07 16:16:06 -0300

    axios implementation

--- src/api/blog-post/controllers/blog-post.js
+++ src/api/blog-post/controllers/blog-post.js
@@ -1,9 +1,38 @@
-'use strict';
+"use strict";
 
 /**
  *  blog-post controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::blog-post.blog-post');
+const getAll = (data) => {
+  return data.map((item) => ({
+    id: item.id,
+    title: item.title,
+    logo: {
+      name: item.logo.name,
+      url: item.logo.url,
+    },
+    links: item.link.map((links) => ({
+      name: links.name,
+    })),
+  }));
+};
+
+module.exports = createCoreController(
+  "api::blog-post.blog-post",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const { query } = ctx;
+
+      const entity = await strapi.entityService.findMany(
+        "api::blog-post.blog-post",
+        {
+          ...query,
+        }
+      );
+      return getAll(entity);
+    },
+  })
+);
