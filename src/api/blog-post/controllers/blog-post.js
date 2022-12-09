2d9086bc3f669f4faec45de485d4412cb5f2528f
gabsrethink <gabriel.melo@rethink.dev>
2022-12-09 15:55:38 -0300

    home page ui

--- src/api/blog-post/controllers/blog-post.js
+++ src/api/blog-post/controllers/blog-post.js
@@ -10,12 +10,29 @@ const getAll = (data) => {
   return data.map((item) => ({
     id: item.id,
     title: item.title,
+    content: item.content,
     logo: {
       name: item.logo.name,
       url: item.logo.url,
     },
+    footerLogo: {
+      name: item.footerLogo.name,
+      url: item.footerLogo.url,
+    },
+    footerInfo: item.footerInfo,
+    trailTitle: item.trailTitle,
+    trailDescription: item.trailDescription,
     links: item.link.map((links) => ({
       name: links.name,
+    })),
+    text_and_image: item.text_and_image.map((posts) => ({
+      title: posts.title,
+      description: posts.description,
+      image: {
+        name: posts.image.name,
+        url: posts.image.url,
+      },
+      is_text_on_left: posts.is_text_on_left,
     })),
   }));
 };
